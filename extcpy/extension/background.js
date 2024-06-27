344631b2023e9fd23080abc9e05caef8aa0fc953
PremanshPareek <premanshpareek@gmail.com>
2024-06-27 09:54:09 +0530

    HA AH

---
+++ extcpy/extension/background.js
@@ -0,0 +1,26 @@
+async function checkUrl(url) {
+  const response = await fetch('http://localhost:5000/check-url', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url: url })
+  });
+  const data = await response.json();
+  return data.is_phishing;
+}
+
+browser.webRequest.onBeforeRequest.addListener(
+  async (details) => {
+    if (details.type === "main_frame") {
+      const isPhishing = await checkUrl(details.url);
+      if (isPhishing) {
+        browser.tabs.create({
+          url: browser.extension.getURL("popup.html")
+        });
+      }
+    }
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
+);
